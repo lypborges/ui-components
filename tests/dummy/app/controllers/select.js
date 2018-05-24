7faca9f943a8e005c51fe6397727a46ed4efdc5c
Felipe Borges Ferreira <lypborges@gmail.com>
2018-05-23 21:58:48 -0300

    Timeout select

--- tests/dummy/app/controllers/select.js
+++ tests/dummy/app/controllers/select.js
@@ -1,16 +1,17 @@
 import Controller from '@ember/controller';
 import {
-  task
+  task, timeout
 } from 'ember-concurrency';
 import { inject as service } from '@ember/service';
 
 export default Controller.extend({
   store: service(),
   searchRemote: task(function*() {
+    yield timeout(800);
     return yield this.get('store').query('user', {}).then((users) => {
       return users;
     });
-  }).drop(),
+  }).restartable(),
   actions: {
     myChangeAction(selected) {
       this.set('selected', selected);
