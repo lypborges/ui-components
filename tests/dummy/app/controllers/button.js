dbd396f25e3620b46ac4d9e665289e83665f9017
Felipe Borges Ferreira <lypborges@gmail.com>
2018-05-24 08:50:27 -0300

    Add text to index

--- tests/dummy/app/controllers/button.js
+++ tests/dummy/app/controllers/button.js
@@ -2,8 +2,7 @@ import Controller from '@ember/controller';
 import { task,timeout } from 'ember-concurrency';
 
 export default Controller.extend({
-  asyncTask: task(function * () {
-   yield timeout(1000);
-   this.set('result', Math.random());
- }).drop(),
+  asyncTask: task(function*() {
+   yield timeout(2000);
+ }).restartable(),
 });
